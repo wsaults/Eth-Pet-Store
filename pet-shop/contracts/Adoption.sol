3d1eba48c810ae272da8b3d3ce3f5ee48ab6b2db
Will Saults <wsaults@yahoo.com>
2018-03-14 19:18:22 -0500

    Initial commit containing pet-shop boilerplate files.

---
+++ pet-shop/contracts/Adoption.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+    address[16] public adopters; // Array of Ethereum addresses
+
+    // Adopting a pet
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
+}
